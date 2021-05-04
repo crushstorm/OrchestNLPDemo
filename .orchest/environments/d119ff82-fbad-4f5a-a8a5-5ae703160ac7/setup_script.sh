5792451a7dd679f83d277dc95fb18e7f4e046a87
Bernando Vialli <kusplus@gmail.com>
2021-05-04 09:38:35 +0000

    pushing .orchest

---
+++ .orchest/environments/d119ff82-fbad-4f5a-a8a5-5ae703160ac7/setup_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install any dependencies you have in this shell script.
+pip install gensim
+pip install nltk
+pip install vaderSentiment-fr
+pip install vaderSentiment
+# E.g. pip install tensorflow
+
